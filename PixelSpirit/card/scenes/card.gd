b0a08976961f289228f2456b2074c060ac257c05
MelPeslier <mel.peslier@gmail.com>
2024-05-02 15:00:05 -0300

    moved

---
+++ PixelSpirit/card/scenes/card.gd
@@ -0,0 +1,37 @@
+@tool
+class_name SpiritCard
+extends Node3D
+
+
+@export var mat: ShaderMaterial : set = _set_mat
+@export var the_name: String : set = _set_the_name
+@export var major_arcana: String : set = _set_major_arcana
+@onready var front: MeshInstance3D = $card/Front
+@onready var major_arcana_number: Label3D = $MajorArcanaNumber
+@onready var card_name: Label3D = $CardName
+
+
+func _ready() -> void:
+	# Call to init the card
+	the_name = the_name
+	major_arcana = major_arcana
+	mat = mat
+
+
+#region Setters
+func _set_mat(_mat: ShaderMaterial) -> void:
+	mat = _mat
+	front.material_override = mat
+
+func _set_the_name(_the_name: String) -> void:
+	the_name = _the_name
+	card_name.text = _the_name
+
+func _set_major_arcana(_major_arcana: String) -> void:
+	major_arcana = _major_arcana
+	if _major_arcana.is_empty():
+		major_arcana_number.text = ""
+		return
+
+	major_arcana_number.text = "- " + _major_arcana + " -"
+#endregion
