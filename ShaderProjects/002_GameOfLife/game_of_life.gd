54718b61c22efc2aac8ef17b329a2eec5b89caef
MelPeslier <mel.peslier@gmail.com>
2024-05-03 02:10:15 -0300

    GameOfLife Shader Grid

--- ShaderProjects/002_GameOfLife/game_of_life.gd
+++ ShaderProjects/002_GameOfLife/game_of_life.gd
@@ -1,2 +1,21 @@
+@tool
 extends ColorRect
 
+var canvas_size : Vector2 : set = _set_canvas_size
+
+@onready var mat : ShaderMaterial = material
+
+
+func _ready() -> void:
+	canvas_size = size
+
+func _process(delta: float) -> void:
+	canvas_size = size
+
+
+func _set_canvas_size(_canvas_size: Vector2) -> void:
+	canvas_size = _canvas_size
+	var _scale = Vector2.ONE
+	_scale.y = canvas_size.y / canvas_size.x
+	_scale.x = canvas_size.x / canvas_size.y
+	mat.set_shader_parameter("scale", _scale)
